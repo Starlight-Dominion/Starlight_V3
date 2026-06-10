29410a05bc65f04a888b037f5366ab1762d537f8
Mungus <thefinestrobert@gmail.com>
2026-06-09 21:46:02 -0400

    production

--- src/Views/app.php
+++ src/Views/app.php
@@ -6,8 +6,8 @@
     <title>Starlight Dominion</title>
     
     <?php if (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'development'): ?>
-        <script type="module" src="http://localhost:5173/@vite/client"></script>
-        <script type="module" src="http://localhost:5173/src/Resources/js/app.js"></script>
+       <!--  <script type="module" src="http://localhost:5173/@vite/client"></script>
+        <script type="module" src="http://localhost:5173/src/Resources/js/app.js"></script> -->
 
         <script type="module" src="http://starlightdominion.com:5173/@vite/client"></script>
         <script type="module" src="http://starlightdominion.com:5173/src/Resources/js/app.js"></script>
